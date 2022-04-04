b375696a6a39ec3016a1b95abf29c0b3612cfb8c
Bart Tange <barttange@gmail.com>
2022-04-04 21:04:25 +0200

    stuff

--- app.py
+++ app.py
@@ -2,41 +2,10 @@
 import aws_cdk as cdk
 from my_pipeline.my_pipeline_stack import MyPipelineStack
 
-import aws_cdk as cdk
-
-from cdk_fargate.cdk_fargate_stack import CdkFargateStack
-
-
-
-
-
-app = cdk.App()
-CdkFargateStack(app, "CdkFargateStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
-app.synth()
-
-
-
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack", 
-    env=cdk.Environment(account="309634134411", region="eu-central-1")
+    env=cdk.Environment(account="911420038161", region="eu-central-1")
 )
 
 app.synth()
